40309eb079706a65743c7b99e2063883af9c19b2
Katie Farrell <kfarrell064@gmail.com>
2017-06-13 14:30:09 +0000

    added invitation

---
+++ ruby-strings-practice.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+guest_name = gets.chomp
+puts "What is the party name?"
+party_name = gets.chomp
+puts "What is the month of the party?"
+month = gets.chomp
+puts "What day of #{month} is the party?"
+day = gets.chomp
+
+puts "Dear #{guest_name},"
+puts "Please come to my party, #{party_name.capitalize}!"
